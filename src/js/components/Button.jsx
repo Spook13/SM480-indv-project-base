9d77232cc110e03b13dd713cb3194ccfc229b7b1
Aly <mckalyn18@gmail.com>
2021-04-10 14:53:35 -0500

    moved dropdown
    moved dropdown menu header.jsx
    attempted adding image

---
+++ src/js/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Button.scss';
+
+function  Button(props){
+
+
+    return (
+        <button className="btn btn-primary circle"  >  
+        </button>
+    )
+}
+ 
+export default Button;
